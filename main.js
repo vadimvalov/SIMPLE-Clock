e436d4b4121f10af2d505a53eeacb334bf0f30f9
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-03-14 02:48:49 +0600

    Основа сайта на html

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./src/index.css";
 
-document.querySelector('#app').innerHTML = `
+/*
+document.querySelector("#body").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <h1> abcd </h1>
   </div>
-`
+`;
+*/
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
