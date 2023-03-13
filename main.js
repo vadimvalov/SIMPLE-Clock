61ec539cdcdeee725b9f8ebb1eaf4ee88526b91f
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-03-14 03:14:56 +0600

    три попытки создать таймер, все тщетны.. пока что.

--- main.js
+++ main.js
@@ -8,4 +8,17 @@ document.querySelector("#body").innerHTML = `
 `;
 */
 
-setupCounter(document.querySelector("#counter"));
+function realtimeClock() {
+  var rtClock = new Date();
+  var hours = rtClock.getHours();
+  var minutes = rtClock.getMinutes();
+  var seconds = rtClock.getSeconds();
+
+  hours = ("0" + hours).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  seconds = ("0" + seconds).slice(-2);
+
+  document.getElementById("clock").innerHTML =
+    hours + " : " + minutes + " : " + seconds;
+  var t = setTimeout(realtimeClock, 500);
+}
